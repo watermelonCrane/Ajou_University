97f4543b5418154a001da4c1556cef03beed87fc
kimjaehak_wsl <kimjaehak512@gmail.com>
2025-10-15 18:37:46 +0900

    탭을 스페이스로 변경

--- Assignment1/sic.asm
+++ Assignment1/sic.asm
@@ -1,20 +1,20 @@
 QUIZ	START	0x000000
 INIT	LDA	ZERO
-	LDX	ZERO
-	STA	LEN
-	STA	TMP
+        LDX	ZERO
+        STA	LEN
+        STA	TMP
 QLOOP	LDCH	STR1, X	. A레지스터에 STR1[X]를 로드
-	COMP	DET	. C'Z'보다 큰지 비교
-	JGT	STEP	. C'Z' 보다 크다면(대문자가 아니라면) QLOOP 이후 STEP으로 점프
-	STX	TMP	. 임시공간에 X값 저장
-	LDX	LEN	. X레지스터에 현재 STR2 길이 로드
-	STCH	STR2, X . STR2에 A의 값 저장 (6번째 줄에서 로드한 값)
-	LDA	LEN
-	ADD	ONE
-	STA	LEN	. LEN++
-	LDX	TMP
+        COMP	DET	. C'Z'보다 큰지 비교
+        JGT	STEP	. C'Z' 보다 크다면(대문자가 아니라면) QLOOP 이후 STEP으로 점프
+        STX	TMP	. 임시공간에 X값 저장
+        LDX	LEN	. X레지스터에 현재 STR2 길이 로드
+        STCH	STR2, X . STR2에 A의 값 저장 (6번째 줄에서 로드한 값)
+        LDA	LEN
+        ADD	ONE
+        STA	LEN	. LEN++
+        LDX	TMP
 STEP	TIX	MAX	. 반복 조건 체크 X가 11일 때까지 반복
-	JLT	QLOOP
+        JLT	QLOOP
 
 
 HALT 	J	HALT
--- Assignment1/xe.asm
+++ Assignment1/xe.asm
@@ -1,21 +1,21 @@
 QUIZ	START	0x000000
 INIT	LDT	#0	. STR2 문자열의 길이
-	LDX	#0
-	
+        LDX	#0
+        
 
 LOOP	LDCH	STR1, X
-	COMP	#90	.C'Z'보다 큰지 비교 (대문자인지 비교함)
-	JGT	STEP
-	RMO	X, S	.S에 X 값 임시 저장
-	RMO	T, X	.X에 T 값 이동
-	STCH	STR2, X	.STR2에 A에 저장된 문자 저장
-	RMO	X, A	.A에 X 값 임시 이동
-	ADD	#1	.A의 값 1증가
-	RMO	A, T	.T에 A 값 저장 (STR2의 문자열 길이 T가 1증가한다.)
-	RMO	S, X	.S에 임시 저장한 값(STR1 인덱스) 다시 X에 저장
+        COMP	#90	.C'Z'보다 큰지 비교 (대문자인지 비교함)
+        JGT	STEP
+        RMO	X, S	.S에 X 값 임시 저장
+        RMO	T, X	.X에 T 값 이동
+        STCH	STR2, X	.STR2에 A에 저장된 문자 저장
+        RMO	X, A	.A에 X 값 임시 이동
+        ADD	#1	.A의 값 1증가
+        RMO	A, T	.T에 A 값 저장 (STR2의 문자열 길이 T가 1증가한다.)
+        RMO	S, X	.S에 임시 저장한 값(STR1 인덱스) 다시 X에 저장
 
 STEP	TIX	MAX	. MAX와 루프 조건 비교
-	JLT	LOOP
+        JLT	LOOP
 
 HALT	J	HALT
 
@@ -23,4 +23,4 @@ STR1 	BYTE	C'syStemPRogrammING'
 STR2	RESB	17	. 최대 17글자 저장가능한 공간 예약
 MAX	WORD	17	. 17번줄 compare시 사용할 값
 
-	END	INIT
+        END	INIT
