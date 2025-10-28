2f04ee662dd1b85c4189205d6665c2e854358bfd
Jaehak-wsl <kimjaehak512@gmail.com>
2025-10-29 04:19:08 +0900

    요구사항 모두 완료

--- HW2/server.js
+++ HW2/server.js
@@ -20,15 +20,17 @@ const RES_FILE = path.join(DATA_DIR, "reservations.json");
 
 // getJSON
 async function loadJSON(filePath) {
-    console.log("in load:", filePath);
+    console.log("loadJson:", filePath);
     const raw = await fs.readFile(filePath, "utf-8");
-    console.log("after load");
+    console.log("load Done");
     return JSON.parse(raw);
 }
 // saveJSON
 async function saveJSON(filePath, data) {
     const json = JSON.stringify(data, null, 4);
+    console.log("saveJSON at ", filePath, json);
     await fs.writeFile(filePath, json, "utf-8");
+    console.log("save done");
 }
 
 
@@ -36,19 +38,22 @@ async function saveJSON(filePath, data) {
 
 // flight 정보 읽기
 async function readFlight() {
+    console.log("read flight info");
     const flight = await loadJSON(FLIGHT_FILE);
     return flight;
 }
 
 // reservations 정보 읽기
 async function readReserv() {
-    console.log("in read");
+    console.log("read Reservation list");
     const reserv = await loadJSON(RES_FILE);
     return reserv;
 }
 
 // reservations 추가하기
 async function updateReserv(newData) {
+    console.log("updating Reservation");
+
     const reserv = await loadJSON(RES_FILE);
     const nextId = reserv[0].nextId;
     reserv[0].nextId++;
@@ -57,15 +62,16 @@ async function updateReserv(newData) {
     reserv.push(newReserv);
     await saveJSON(RES_FILE, reserv);
 
-    // return reserv;
+    console.log("update done");
+
+    return newReserv;
 }
 
 // reservations 삭제하기
 async function deleteReserv(id) {
+    console.log("deletion: ", id);
+
     const reserv = await loadJSON(RES_FILE);
-
-    console.log(typeof reserv, typeof id);
-
     const target = reserv.find(data => data.id === parseInt(id));
 
     console.log(target);
@@ -92,17 +98,13 @@ async function seatRangeOK(seat) {
 
     const flight = await readFlight();    // flight 파일 읽어오기
 
-    console.log('in', row);
+    //console.log('in', row);
 
-    if (row <= 0 || row > flight.rows) {     //row의 범위가 벗어난다면
-        //console.log('detect!');
-        return false;
-    }
-    if (!flight.cols.includes(col)) {       //col의 범위가 벗어난다면
-        return false;
+    if (row <= 0 || row > flight.rows || !flight.cols.includes(col)) {     // 범위가 벗어난다면
+        throw { status: 400, message:`좌석 범위를 벗어낫습니다. 유효 범위: 행 1-${flight.rows} 열 ${flight.cols.join(", ")}`, };
     }
 
-    console.log("done range");
+    console.log("done seatRangeOK");
     return true;
 }
 
@@ -111,7 +113,7 @@ async function seatReservOK(seat) {
     const reserv = await readReserv();    // reservations 파일 읽어오기
 
     if (reserv.find((e) => e.seat === seat) === undefined) return true;
-    else return false;
+    else throw { status: 409, message: `이미 예약된 좌석입니다: ${seat}` };
 }
 
 
@@ -123,7 +125,7 @@ app.get('/api/flight', async (req, res) => {
 });
 
 app.get('/api/reservations', async (req, res) => {
-    console.log("in get");
+    //console.log("in get");
     const reserv = await readReserv();
     res.json(reserv);
 });
@@ -132,12 +134,12 @@ app.post('/api/reservations', async (req, res, next) => {
     let { name, seat } = req.body;
     name = name.trim(); //앞뒤 공백제거
 
-    console.log(req.body);
+    //console.log(req.body);
 
     try {
         // 좌석 형식 검사
-        if (name.length < 2) throw { status: 400, message: "400 Bad Request, 이름을 더 길게 해주세요." };    //길이 체크
-        if (!seatPatternOK(seat)) throw { status: 400, message: "400 Bad Request, 좌석 형식을 확인해주세요." };  //좌석 형식 체크
+        if (name.length < 2) throw { status: 400, message: "이름을 2자 이상 입력하세요." };    //길이 체크
+        if (!seatPatternOK(seat)) throw { status: 400, message: "좌석 형식이 올바르지 않습니다. 예: 3C / 12A" };  //좌석 형식 체크
 
         // 좌석 형식에 맞게 다시 작성 ex) 01A -> 1A
         const seatRow = String(parseInt(seat.slice(0, -1), 10));
@@ -145,17 +147,16 @@ app.post('/api/reservations', async (req, res, next) => {
         seat = seatRow + seatCol;
 
         // 좌석 범위 체크, 중복 체크
-        if (!await seatRangeOK(seat)) throw { status: 400, message: "400 Bad Request, 가능한 좌석 범위를 초과했습니다." }; // 좌석 범위 체크
-        if (!await seatReservOK(seat)) throw { status: 409, message: "409 Conflict, 이미 예약된 좌석입니다." };    // 빈좌석 체크
+        await seatRangeOK(seat);     // 좌석 범위 체크
+        await seatReservOK(seat);    // 빈좌석 체크
     } catch (err) {
         return next(err);
     }
 
-    console.log("검사 완료");
+    console.log("좌석 검사 완료");
 
     // 좌석 예약
-    const newReserv = { name: name, seat: seat, ts: new Date().toISOString() };
-    await updateReserv(newReserv);
+    const newReserv = await updateReserv({ name: name, seat: seat, ts: new Date().toISOString() });
 
     return res.status(201).json(newReserv);     //예약한 좌석 정보 반환
 
@@ -177,10 +178,14 @@ app.delete('/api/reservations/:id', (req, res) => {
 
 
 //에러 핸들러
+app.use((req, res) => {
+    res.status(404).send("잘못된 주소");
+})
+
 app.use((err, req, res, next) => {
     //console.dir(err);
 
-    res.status(err.status).send(`<h1>${err.message}</h1>`);
+    res.status(err.status).send(err.message);
 
 })
 
